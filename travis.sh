72cca63284ebf10e3e6fa3cdedf559df5a119092
Demyx <info@demyx.sh>
2020-04-13 17:01:45 -0700

    Commit version file first before committing the rest

--- travis.sh
+++ travis.sh
@@ -21,7 +21,13 @@ DEMYX_MARIADB_VERSION=$DEMYX_MARIADB_VERSION" > VERSION
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote set-url origin https://${DEMYX_GITHUB_TOKEN}@github.com/demyxco/"$DEMYX_REPOSITORY".git
-git add .; git commit -m "Travis Build $TRAVIS_BUILD_NUMBER"; git push origin HEAD:master
+# Commit VERSION first
+git add VERSION
+git commit -m "ALPINE $DEMYX_ALPINE_VERSION, MARIADB $DEMYX_MARIADB_VERSION"
+# Commit the rest
+git add .
+git commit -m "Travis Build $TRAVIS_BUILD_NUMBER"
+git push origin HEAD:master
 
 # Set the default path to README.md
 README_FILEPATH="./README.md"
